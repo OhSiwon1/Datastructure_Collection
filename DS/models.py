d5aaf5d024189309a1c7174369224442f8af2399
OhSiwon <23-117@ksa.hs.kr>
2024-05-25 22:34:10 +0900

    엥

--- DS/models.py
+++ DS/models.py
@@ -19,15 +19,15 @@ class Project(models.Model):
     content=models.TextField(blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_project')
     year=models.ForeignKey(Year,on_delete=models.CASCADE)
-    uploadedFile = models.FileField(upload_to="zipfile/")
+    uploadedFile = models.FileField(upload_to="zipfile/",null=True)
     harts = models.ManyToManyField(User, related_name='hart_project',blank=True)  # 추천인 추가
     video=models.URLField()
     imgfile = models.ImageField(null=True, upload_to="") # 이미지 컬럼 추가
     abstract=models.CharField(max_length=100,blank=True)
     hashtag=models.ManyToManyField(Hashtag,related_name='project_hashtag',blank=True)
     member=models.CharField(max_length=100)
-    poster=models.FileField(upload_to="paperfile/",blank=True)
-    repoter=models.FileField(upload_to="repoter/",blank=True,null=True)
+    poster=models.FileField(upload_to="paperfile/",null=True)
+    repoter=models.FileField(upload_to="repoter/",null=True)
 
     def __str__(self):
         return self.title
